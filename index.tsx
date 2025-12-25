edf42065246b491746d5fa6c0eeae33c3c9ca808
Ioannis Kokkalis <ioanniskokkalis2001@gmail.com>
2025-12-26 01:45:58 +0200

    refactor: Switch from BrowserRouter to HashRouter for routing.

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import OAuthCallback from './pages/OAuthCallback';
 
@@ -12,11 +12,11 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/oauth/callback" element={<OAuthCallback />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
